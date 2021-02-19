db80288eec1e442a5b3b16d42fa54e8fe53c1fb0
anuj <74948201+anujb6@users.noreply.github.com>
2021-02-19 23:47:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,22 +27,22 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      var tom2 = new Audio("tom-2.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom3 = new Audio('tom-3.mp3');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
+      var tom4 = new Audio('tom-4.mp3');
       tom4.play();
       break;
 
